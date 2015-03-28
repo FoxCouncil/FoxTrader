723cedd69e1eda1d7ec5e88aa7cb4fc260bb19c4
Fox Diller <onefox+github@gmail.com>
2015-03-28 14:10:23 -0700

    It's all relative.

--- src/pch.h
+++ src/pch.h
@@ -38,10 +38,10 @@
 #include <SDL2/SDL_opengl_glext.h>
 
 // SDL Libs
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 
 // Slow SDL Functionality
 /**
